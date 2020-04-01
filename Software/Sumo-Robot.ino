d82dc4a1b98eb5094ca6907d3c6cf87177bb4135
Tiger Chen <tiger98c@gmail.com>
2020-04-02 04:08:31 +1100

    decently working calibrated version with some behaviours disabled due to bugs

--- Software/Sumo-Robot.ino
+++ Software/Sumo-Robot.ino
@@ -25,20 +25,36 @@ uint16_t sensorValues[SensorCount];
 const int echoPin = 8;
 const int trigPin = 9;
 
+bool calibrated = false;
+bool basicMode = false;
+
 //cpr 2400 is equivalent to a unit of distance which is  proportional to the diameter of the wheels
 const float CPR = 2400.0f;
-float vel = 15000.0f; 
+float vel = 15000.0f;
+float velHigh = vel * 1.5;
+float velMedium = vel;
+float velLow = vel * 0.7;
 
-float inverseRev = 0.3f;
-float turnRev = 4.6f;
-float reverseRev = 3.1f;
+float forwardRev = 1.0f;
+float forwardBoundRev = 0.4f;
+float inverseRev = 1.5f;
+float turnRev = 1.6f;
+float reverseRev = 1.1f;
+float scanRev = 3.0f;
+float tornadoRev = 7.0f;
+float snakeRev = 0.2f;
+
+unsigned long forwardDuration = 0;
+unsigned long forwardBoundDuration = 0;
 unsigned long inverseDuration = 0;
 unsigned long turnDuration = 0;
 unsigned long reverseDuration = 0;
-unsigned long tornadoDuration = 2000;
+unsigned long scanDuration = 0;
+unsigned long tornadoDuration = 0;
+unsigned long snakeDuration = 0;
 
 unsigned long infraredThreshold = 500;
-long distThreshold = 20;
+long distThreshold = 70;
 bool invert = false;
 
 // Generally, you should use "unsigned long" for variables that hold time
@@ -54,9 +70,9 @@ const unsigned long TIMEOUT = 200;
 enum game_state_t { GS_IDLE,
                     GS_REMOTE_CONTROL,
                     GS_BATTLE_MAIN,
+                    GS_BATTLE_CHARGE,
                     GS_BATTLE_EDGE,
                     GS_BATTLE_SCAN,
-                    GS_BATTLE_TARGET,
                     GS_BATTLE_TORNADO,
                     GS_BATTLE_SNAKE};
 game_state_t game_state = GS_IDLE;
@@ -80,6 +96,8 @@ void setup() {
   Serial.println("Setting parameters...");
   
   updateTimingParams();
+  setVelocityMax(30000.0f);
+  setCurrentMax(30.0f);
 
   help();
 }
@@ -94,10 +112,12 @@ void loop() {
     counterActive = false;
     eventTrigger = true;
   }
-
+  
   switch(game_state) {
     case GS_IDLE:
-      
+      if (move_state != MS_IDLE) {
+        brake();
+      }
       break;
     case GS_REMOTE_CONTROL:
       if (move_state != MS_IDLE && eventTrigger) {
@@ -108,34 +128,147 @@ void loop() {
       qtr.read(sensorValues);
       bool rightEdgeTrig = (sensorValues[0] > infraredThreshold && invert) || (sensorValues[0] < infraredThreshold && !invert);
       bool leftEdgeTrig = (sensorValues[1] > infraredThreshold && invert) || (sensorValues[1] < infraredThreshold && !invert);
-      game_state = (rightEdgeTrig || leftEdgeTrig) ? GS_BATTLE_EDGE : game_state;
-      
-      bool distTrig = distThreshold < distance();
+      bool edgeTrig = rightEdgeTrig || leftEdgeTrig;
+      game_state = (edgeTrig) ? GS_BATTLE_EDGE : game_state;
+
+//      if (!edgeTrig && game_state != GS_BATTLE_EDGE) {
+//        if (distance() < distThreshold && game_state != GS_BATTLE_CHARGE) {
+//          Serial.println(F("TARGET SPOTTED!"));
+//          game_state = GS_BATTLE_CHARGE;
+//        }
+//      }
       
       if (game_state == GS_BATTLE_MAIN) {
-        
-      }
-      else if (game_state == GS_BATTLE_EDGE) {
-        if (rightEdgeTrig && leftEdgeTrig) {
-          if (random(1)) {
-            right(vel);
+        if (move_state != MS_FORWARD) {
+          Serial.println(F("FORWARD"));
+          forward(velMedium);
+          if (!basicMode) {
+            setTimer(random(forwardDuration - forwardBoundDuration, forwardDuration + forwardBoundDuration));
           }
-          else {
-            left(vel);
+        }
+        else if (eventTrigger) {
+          long randNumber = random(100);
+          if (randNumber < 10) { //turn
+            game_state = GS_BATTLE_EDGE;
           }
-          
+          else if (randNumber < 30) { //scan
+            game_state = GS_BATTLE_SCAN;
+          }
+          else if (randNumber < 45) { //tornado
+            game_state = GS_BATTLE_TORNADO;
+          }
+//          else if (randNumber < 90) { //snake
+//            game_state = GS_BATTLE_SNAKE;
+//          }
+          else { //charge
+            game_state = GS_BATTLE_CHARGE;
+          }
         }
       }
-      else if (game_state == GS_BATTLE_SCAN) {
-        
+      else if (game_state == GS_BATTLE_CHARGE) {
+        if (move_state != MS_FORWARD) {
+          Serial.println(F("CHARGE"));
+          forward(velMedium);
+        }
       }
-      else if (game_state == GS_BATTLE_TARGET) {
-        
+      else if (game_state == GS_BATTLE_EDGE) {
+          if (move_state == MS_FORWARD) {
+            Serial.println(F("EDGE"));
+            hardBrake();
+          }
+          
+          if ((rightEdgeTrig && leftEdgeTrig)) {
+            if (random(2)) {
+              right(velLow);
+            }
+            else {
+              left(velLow);
+            }
+          }
+          else if (rightEdgeTrig) {
+            left(velLow);
+          }
+          else if (leftEdgeTrig) {
+            right(velLow);
+          }
+          delay(turnDuration);
+          brake();
+          game_state = GS_BATTLE_MAIN;
+      }
+      else if (game_state == GS_BATTLE_SCAN) {
+        if (move_state != MS_RIGHT && move_state != MS_LEFT) {
+          Serial.println(F("SCAN"));
+          if (random(2)) {
+            right(velLow);
+          }
+          else {
+            left(velLow);
+          }
+          setTimer(scanDuration);
+        }
+        setVelocity(0,0);
+        delay(100);
+        bool distTrig = distance() < distThreshold;
+        if (distTrig) {
+          Serial.println(F("TARGET SPOTTED!"));
+          game_state = GS_BATTLE_CHARGE;
+        }
+        else {
+          if (eventTrigger) {
+            brake();
+            game_state = GS_BATTLE_MAIN;
+          }
+          else if (move_state == MS_RIGHT) {
+            right(velLow);
+            delay(100);
+          }
+          else if (move_state == MS_LEFT) {
+            left(velLow);
+            delay(100);
+          }
+        }
       }
       else if (game_state == GS_BATTLE_TORNADO) {
+        if (move_state != MS_RIGHT && move_state != MS_LEFT) {
+          Serial.println(F("TORNADO"));
+          if (random(1)) {
+            right(velHigh);
+          }
+          else {
+            left(velHigh);
+          }
+          setTimer(tornadoDuration);
+        }
         
+        else if (eventTrigger) {
+          hardBrake();
+          game_state = GS_BATTLE_MAIN;
+        }
       }
       else if (game_state == GS_BATTLE_SNAKE) {
+        if (move_state != MS_RIGHT && move_state != MS_LEFT) {
+          Serial.println(F("SNAKE"));
+          if (random(1)) {
+            move_state = MS_RIGHT;
+            setVelocity(velMedium, velLow);
+          }
+          else {
+            move_state = MS_LEFT;
+            setVelocity(velLow, velMedium);
+          }
+          setTimer(snakeDuration);
+        }
+        else if (eventTrigger) {
+          if (move_state == MS_RIGHT) {
+            move_state = MS_LEFT;
+            setVelocity(velLow, velMedium);
+          }
+          else {
+            move_state = MS_RIGHT;
+            setVelocity(velMedium, velLow);
+          }
+          setTimer(snakeDuration);
+        }
         
       }
       break;
@@ -178,9 +311,15 @@ void serialEvent() {
         delay(inputParam.toFloat());
         hardBrake();
       }
-      else if (inputCommand == "set_vel" || inputCommand == "set_inverse" || inputCommand == "set_turn" || inputCommand == "set_reverse") {
+      else if (inputCommand == "set_vel" || inputCommand == "set_forward" || inputCommand == "set_forward_bound" || inputCommand == "set_inverse" || inputCommand == "set_turn" || inputCommand == "set_reverse" || inputCommand == "set_scan" || inputCommand == "set_tornado" || inputCommand == "set_snake") {
         if (inputCommand == "set_vel") {
           vel = inputParam.toFloat();
+        }
+        else if (inputCommand == "set_forward") {
+          forwardRev = inputParam.toFloat();
+        }
+        else if (inputCommand == "set_forward_bound") {
+          forwardBoundRev = inputParam.toFloat();
         }
         else if (inputCommand == "set_inverse") {
           inverseRev = inputParam.toFloat();
@@ -191,8 +330,27 @@ void serialEvent() {
         else if (inputCommand == "set_reverse") {
           reverseRev = inputParam.toFloat();
         }
+        else if (inputCommand == "set_scan") {
+          scanRev = inputParam.toFloat();
+        }
+        else if (inputCommand == "set_tornado") {
+          tornadoRev = inputParam.toFloat();
+        }
+        else if (inputCommand == "set_snake") {
+          snakeRev = inputParam.toFloat();
+        }
         updateTimingParams();
         Serial.println(F("Parameter updated!"));
+      }
+      else if (inputCommand == "set_basicMode") {
+        if (inputParam.toInt()) {
+          Serial.println(F("Basic Mode ON"));
+          basicMode = true;
+        }
+        else {
+          Serial.println(F("Basic Mode OFF"));
+          basicMode = false;
+        }
       }
       else if (inputCommand == "set_infraredThreshold") {
         infraredThreshold = inputParam.toInt();
@@ -238,7 +396,21 @@ void serialEvent() {
       }
       else if (inputCommand == "max_vel") {
         setVelocityMax(inputParam.toFloat());
-        
+        Serial.println(F("Max velocity updated!"));
+      }
+      else if (inputCommand == "pre_calibrate") {
+        Serial.println(F("DOES NOT WORK - Setting pre-calibration status!"));
+        for (int axis = 0; axis < 2; ++axis) {
+          if (inputParam == "1"){
+            odrive_serial << "w axis" << axis << ".motor.config.pre_calibrated " << true << '\n';
+          }
+          else if (inputParam == "0"){
+            odrive_serial << "w axis" << axis << ".motor.config.pre_calibrated " << false << '\n';
+          }
+          else {
+            Serial.println(F("Error: Invalid boolean, put a \'1\' or \'0\'."));
+          }
+        }
       }
       else {
         Serial.println("Error: " + inputCommand + " is an invalid command...");
@@ -247,10 +419,15 @@ void serialEvent() {
     else { //operations that do not need a numerical argument
       if (inputCommand == "info") {
         Serial << "Velocity: " << vel << ", IR Threshold: " << infraredThreshold << ", Distance Threshold: " << distThreshold << '\n';
-        
-        Serial << "Inverse revolutions: " << inverseRev << ", Inverse Duration: " << inverseDuration << '\n';
-        Serial << "Turn revolutions: " << turnRev << ", Turn Duration: " << turnDuration << '\n';
-        Serial << "Reverse revolutions: " << reverseRev <<", Reverse Duration: " << reverseDuration << '\n';
+
+        Serial << F("Forward revolutions: ") << forwardRev << F(", Forward Duration: ") << forwardDuration << '\n';
+        Serial << F("Forward bound revolutions: ") << forwardBoundRev << F(", Forward bound Duration: ") << forwardBoundDuration << '\n';
+        Serial << F("Inverse revolutions: ") << inverseRev << F(", Inverse Duration: ") << inverseDuration << '\n';
+        Serial << F("Turn revolutions: ") << turnRev << F(", Turn Duration: ") << turnDuration << '\n';
+        Serial << F("Reverse revolutions: ") << reverseRev << F(", Reverse Duration: ") << reverseDuration << '\n';
+        Serial << F("Scan revolutions: ") << scanRev << F(", Scan Duration: ") << scanDuration << '\n';
+        Serial << F("Tornado revolutions: ") << tornadoRev << F(", Tornado Duration: ") << tornadoDuration << '\n';
+        Serial << F("Snake revolutions: ") << snakeRev << F(", Snake Duration: ") << snakeDuration << '\n';
         
         
         Serial << "Distance: " << distance() << "cm\n";
@@ -281,33 +458,37 @@ void serialEvent() {
         help();
       }
       else if (inputCommand == "calibrate") {
-        motorCalibration();
+        calibration();
+      }
+      else if (inputCommand == "full_calibrate") {
+        fullCalibration();
       }
       else if (inputCommand == "save") {
-        Serial.println(F("Saved config!"));
+        Serial.println(F("DOES NOT WORK - Saved config!"));
         odrive_serial << "ss\n";
       }
       else if (inputCommand == "erase") {
-        Serial.println(F("Erased config!"));
+        Serial.println(F("DOES NOT WORK - Erased config!"));
         odrive_serial << "se\n";
       }
       else if (inputCommand == "reboot") {
-        Serial.println(F("Rebooting odrive!"));
+        Serial.println(F("DOES NOT WORK - Rebooting odrive!"));
         odrive_serial << "sr\n";
       }
-      else if (inputCommand == "remote_control") {
+      else if (inputCommand == "remote_control" && calibrated) {
         game_state = GS_REMOTE_CONTROL;
         Serial.println(F("Mode: Remote Control"));
       }
-      else if (inputCommand == "start") {
-        delay(5000);
+      else if (inputCommand == "start" && calibrated) {
+        
         for (int i = 5; i >= 0; i--) {
           Serial << F("Countdown: ") << i << '\n';
+          delay(1000);
         }
         game_state = GS_BATTLE_MAIN;
         Serial.println(F("Mode: Battle"));
       }
-      else if (inputCommand == "quick_start") {
+      else if (inputCommand == "quick_start" && calibrated) {
         game_state = GS_BATTLE_MAIN;
         Serial.println(F("Mode: Battle"));
       }
@@ -329,7 +510,7 @@ void serialEvent() {
          Serial.println(F("Invert updated!"));
       }
       else {
-        Serial.println("Error: " + inputCommand + " is an invalid command...");
+        Serial.println("Error: " + inputCommand + " is an invalid command or odrive is uncalibrated...");
       }
     }
     
@@ -348,13 +529,14 @@ void help() {
   Serial.println(F("Send the string...'calibrate','save','erase','reboot'"));
   Serial.println(F("Send the string...'pos_gain','vel_gain','vel_integrator_gain' [float]"));
   Serial.println(F("Send the string...'set_resistance','set_inductance' [float]"));
-  Serial.println(F("Send the string...'max_vel','max_current' [float]\n"));
+  Serial.println(F("Send the string...'max_vel','max_current' [float]"));
+  Serial.println(F("Send the string...'pre_calibrate' [1,0]"));
+  Serial.println(F("Send the string...'calibrate','full_calibrate','save','erase','reboot'\n"));
   
   Serial.println(F("Parameters Command List..."));
-  Serial.println(F("Send the string...'set_vel','set_inverse','set_turn','set_reverse' [float]"));
+  Serial.println(F("Send the string...'set_vel','set_forward','set_forward_bound','set_inverse','set_turn','set_reverse','set_scan','set_tornado','set_snake' [float]"));
   Serial.println(F("Send the string...'set_infraredThreshold','set_distThreshold' [integer]"));
   Serial.println(F("Send the string...'set_resistance','set_inductance' [float]"));
-  Serial.println(F("Send the string...'max_vel','max_current' [float]"));
   Serial.println(F("Send the string...'invert'\n"));
   
   Serial.println(F("Movement Command List..."));
@@ -367,13 +549,22 @@ void help() {
 
 
 void updateTimingParams() {
-  inverseDuration = revolutionsToMicroseconds(CPR, vel, inverseRev);
-  turnDuration = revolutionsToMicroseconds(CPR, vel, turnRev);
-  reverseDuration = revolutionsToMicroseconds(CPR, vel, reverseRev);
+  float velHigh = vel * 1.5;
+  float velMedium = vel;
+  float velLow = vel * 0.7;
+  
+  forwardDuration = revolutionsToMicroseconds(CPR, velMedium, forwardRev);
+  forwardBoundDuration = revolutionsToMicroseconds(CPR, velMedium, forwardBoundRev);
+  inverseDuration = revolutionsToMicroseconds(CPR, velMedium, inverseRev);
+  turnDuration = revolutionsToMicroseconds(CPR, velLow, turnRev);
+  reverseDuration = revolutionsToMicroseconds(CPR, velMedium, reverseRev);
+  scanDuration = revolutionsToMicroseconds(CPR, velLow, scanRev);
+  tornadoDuration = revolutionsToMicroseconds(CPR, velHigh, tornadoRev);
+  snakeDuration = revolutionsToMicroseconds(CPR, velMedium, snakeRev);
 }
 
-unsigned long revolutionsToMicroseconds(float cpr, float vel, float rev) {
-  return ((cpr * rev) / vel) * 1000.0f;
+unsigned long revolutionsToMicroseconds(float cpr, float vel_arg, float rev) {
+  return ((cpr * rev) / vel_arg) * 1000.0f;
 }
 
 float odriveAxisReadFloat(int axis, String property) {
@@ -385,7 +576,25 @@ void odriveAxisWriteFloat(int axis, String property, float val) {
   odrive_serial << "w axis" << axis << property << ' ' << val << '\n';
 }
 
-void motorCalibration() {
+void calibration() {
+  for (int motornum = 0; motornum < 2; motornum++) {
+    char c = motornum + '0';
+    int requested_state;
+    
+    requested_state = ODriveArduino::AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
+    Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+    odrive.run_state(motornum, requested_state, true);
+    
+    requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
+    Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+    odrive.run_state(motornum, requested_state, false); // don't wait
+
+    calibrated = true;
+  }
+  
+}
+
+void fullCalibration() {
   for (int motornum = 0; motornum < 2; motornum++) {
     char c = motornum + '0';
     int requested_state;
@@ -401,13 +610,15 @@ void motorCalibration() {
     requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
     Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
     odrive.run_state(motornum, requested_state, false); // don't wait
+
+    calibrated = true;
   }
 }
 
-void setVelocityMax(float vel) {
+void setVelocityMax(float vel_arg) {
   for (int axis = 0; axis < 2; ++axis) {
-    odriveAxisWriteFloat(axis, ".controller.config.vel_limit", vel);
-//    odrive_serial << "w axis" << axis << ".controller.config.vel_limit " << vel << '\n';
+    odriveAxisWriteFloat(axis, ".controller.config.vel_limit", vel_arg);
+//    odrive_serial << "w axis" << axis << ".controller.config.vel_limit " << vel_arg << '\n';
   }
 }
 
@@ -424,21 +635,21 @@ void setTimer(unsigned long duration) {
   counterActive = true;
 }
 
-void moveAction(String action, float vel) {
+void moveAction(String action, float vel_arg) {
   if (inputCommand == "forward") {
-    forward(vel);
+    forward(vel_arg);
     Serial.println("Forward OK");
   } 
   else if (inputCommand == "left") {
-    left(vel);
+    left(vel_arg);
     Serial.println("Left OK");
   }
   else if (inputCommand == "right") {
-    right(vel);
+    right(vel_arg);
     Serial.println("Right OK");
   }
   else if (inputCommand == "reverse") {
-    reverse(vel);
+    reverse(vel_arg);
     Serial.println("Reverse OK");
   }
 }
@@ -448,24 +659,24 @@ void setVelocity(float velAxis0, float velAxis1) {
   odrive.SetVelocity(1, velAxis1);
 }
 
-void forward(float vel) {
+void forward(float vel_arg) {
   move_state = MS_FORWARD;
-  setVelocity(vel, vel);
+  setVelocity(vel_arg, vel_arg);
 }
 
-void right(float vel) {
+void right(float vel_arg) {
   move_state = MS_RIGHT;
-  setVelocity(vel, - vel);
+  setVelocity(vel_arg, - vel_arg);
 }
 
-void left(float vel) {
+void left(float vel_arg) {
   move_state = MS_LEFT;
-  setVelocity(-vel, vel);
+  setVelocity(-vel_arg, vel_arg);
 }
 
-void reverse(float vel) {
+void reverse(float vel_arg) {
   move_state = MS_REVERSE;
-  setVelocity(-vel, -vel);
+  setVelocity(-vel_arg, -vel_arg);
 }
 
 void brake() {
@@ -481,12 +692,11 @@ void hardBrake() {
 }
 
 void inverseMove() {
-  if (move_state == MS_FORWARD) reverse(vel);
-  else if (move_state == MS_LEFT) right(vel);
-  else if (move_state == MS_RIGHT) left(vel);
-  else if (move_state == MS_REVERSE) forward(vel);
+  if (move_state == MS_FORWARD) reverse(velHigh);
+  else if (move_state == MS_LEFT) right(velHigh);
+  else if (move_state == MS_RIGHT) left(velHigh);
+  else if (move_state == MS_REVERSE) forward(velHigh);
 }
-
 
 long pingDuration() {
   digitalWrite(trigPin, LOW);
